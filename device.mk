29a21afa24e7497b0df05bd5b7b8e012009f1699
XiNGRZ <chenxingyu92@gmail.com>
2020-02-13 09:53:06 +0800

    m1923: Use charger from stock
    Change-Id: Ib1117d65d76bd16436395f266184f9b46325b162

--- device.mk
+++ device.mk
@@ -18,6 +18,10 @@ PRODUCT_PACKAGES += \
 TARGET_SCREEN_HEIGHT := 2244
 TARGET_SCREEN_WIDTH := 1080
 
+# Charger
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/charger,root/res/images/charger)
+
 # Init
 PRODUCT_PACKAGES += \
     init.target.rc
