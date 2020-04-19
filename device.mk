b52d04a4b8ddd9f2aed71d3c8cb986616fcee14e
XiNGRZ <chenxingyu92@gmail.com>
2020-04-19 13:03:04 +0800

    m1923: overlay: Add NoCutoutOverlay
    Change-Id: I5f89570250780e7b1d99faae38c38e723831d83a

--- device.mk
+++ device.mk
@@ -11,6 +11,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2244
 TARGET_SCREEN_WIDTH := 1080
